05a435c36e4f212b144cad045a6bcaa48bb77c92
Thomas Stastny <thomas.stastny@mavt.ethz.ch>
2016-12-09 18:37:08 +0100

    small weighting update

--- nmpc_3dhl/sim_nmpc.m
+++ nmpc_3dhl/sim_nmpc.m
@@ -87,8 +87,8 @@ zref        = [0, ic_u, ic_u];
 Q_scale     = [1 1 1 1, 1 1 1, 1, 50*pi/180 50*pi/180 50*pi/180, 1];
 R_scale     = [1, 1 30*pi/180 15*pi/180, 1 5*pi/180 5*pi/180];
 
-Q_output    = [70 200 5 10 10 10 1 5 20 20 5 1]./Q_scale.^2;
-QN_output   = [70 200 5 10 10 10 1 5 20 20 5 1]./Q_scale.^2;
+Q_output    = [100 200 5 5 10 10 1 5 20 20 5 1]./Q_scale.^2;
+QN_output   = [100 200 5 5 10 10 1 5 20 20 5 1]./Q_scale.^2;
 R_controls  = [1 0.1 0.1 0.1 0 10 10]./R_scale.^2;
 
 Q_prev      = [(linspace(1,0,N+1)').^0,...
