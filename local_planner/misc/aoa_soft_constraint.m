a348dc2df039f19f5c61bf9effc0ff1d3a518635
Thomas Stastny <thomas.stastny@mavt.ethz.ch>
2020-03-27 21:26:28 +0100

    play with soft constraints

--- local_planner/misc/aoa_soft_constraint.m
+++ local_planner/misc/aoa_soft_constraint.m
@@ -2,36 +2,36 @@ clear;
 clc;
 close all;
 
-aoa_set = linspace(-10,13,1001);
+aoa_set = linspace(-5.5,8.5,1001);
 aoa_m = -5;
 aoa_p = 8;
 delta_aoa = 2;
 sig_aoa_1 = 0.01; % MUST be > 0
-w_sig_aoa = 5;
+w_sig_aoa = 1;
 
 % weight dependent scaling
 k_aoa = log(w_sig_aoa/sig_aoa_1);
 
 % exponential cost
-sig_aoa_p = w_sig_aoa*exp((aoa_set - aoa_p)/delta_aoa*k_aoa);
+sig_aoa_p = w_sig_aoa*exp(-(-aoa_set - -aoa_p)/delta_aoa*k_aoa);
 sig_aoa_m = w_sig_aoa*exp(-(aoa_set - aoa_m)/delta_aoa*k_aoa);
 
 % linearize cost beyond zero point
-sig_aoa_p(aoa_set - aoa_p > 0) = w_sig_aoa * (1.0 + k_aoa/delta_aoa * (aoa_set(aoa_set - aoa_p > 0) - aoa_p));
+sig_aoa_p(-aoa_set - -aoa_p < 0) = w_sig_aoa * (1.0 + -k_aoa/delta_aoa * (-aoa_set(-aoa_set - -aoa_p < 0) - -aoa_p));
 sig_aoa_m(aoa_set - aoa_m < 0) = w_sig_aoa * (1.0 + -k_aoa/delta_aoa * (aoa_set(aoa_set - aoa_m < 0) - aoa_m));
 
 sig_aoa = sig_aoa_p + sig_aoa_m;
 
 % exponential jacobians
 t2 = 1.0/delta_aoa; 
-t3 = k_aoa*sig_aoa_m*t2; 
+t3 = k_aoa*-sig_aoa_m*t2; 
 jac_gamma = t3-k_aoa*sig_aoa_p.*t2; 
 jac_theta = -t3+k_aoa*sig_aoa_p.*t2;
 
 % linearized jacobians
-jac_gamma(aoa_set - aoa_p > 0) = w_sig_aoa * -k_aoa/delta_aoa;
-jac_gamma(aoa_set - aoa_m < 0) = w_sig_aoa * k_aoa/delta_aoa;
-jac_theta(aoa_set - aoa_p > 0) = w_sig_aoa * k_aoa/delta_aoa;
+jac_gamma(-aoa_set - -aoa_p < 0) = w_sig_aoa * -k_aoa/delta_aoa;
+jac_gamma(aoa_set - aoa_m < 0) = w_sig_aoa * -k_aoa/delta_aoa;
+jac_theta(-aoa_set - -aoa_p < 0) = w_sig_aoa * -k_aoa/delta_aoa;
 jac_theta(aoa_set - aoa_m < 0) = w_sig_aoa * -k_aoa/delta_aoa;
 
 figure('color','w');
@@ -48,13 +48,13 @@ xlim(aoa_set([1 end]));
 subplot(2,2,2); hold on; grid on; box on;
 plot(aoa_set, jac_gamma);
 xlabel('\alpha [deg]');
-ylabel('d(\sigma)/d(\gamma)');
+ylabel('d\sigma/d\gamma');
 
 xlim(aoa_set([1 end]));
 
 subplot(2,2,4); hold on; grid on; box on;
 plot(aoa_set, jac_theta);
 xlabel('\alpha [deg]');
-ylabel('d(\sigma)/d(\theta)');
+ylabel('d\sigma/d\theta');
 
 xlim(aoa_set([1 end]));
--- local_planner/misc/exponential_soft_constraint.m
+++ local_planner/misc/exponential_soft_constraint.m
@@ -16,17 +16,19 @@ for i = 1:length(iset)
         normalized_state = x_set - x_off;
 %         sig_h_exp(normalized_state<0,i,j) = jset(j)*(sqrt(1-normalized_state(normalized_state<0)*log(jset(j)*iset(i)))).^2;
 %         sig_h_exp(normalized_state>=0,i,j) = jset(j)*(sqrt(exp(-normalized_state(normalized_state>=0)*log(jset(j)*iset(i))))).^2;
-        sig_h_exp(normalized_state<0,i,j) = jset(j)*(1-normalized_state(normalized_state<0)*log(jset(j)*sqrt(iset(i)))).^2;
-        sig_h_exp(normalized_state>=0,i,j) = jset(j)*(exp(-normalized_state(normalized_state>=0)*log(jset(j)*sqrt(iset(i))))).^2;
+        sig_h_exp(normalized_state<0,i,j) = jset(j)*(1-normalized_state(normalized_state<0)*log(sqrt(jset(j)*iset(i)))).^2;
+        sig_h_exp(normalized_state>=0,i,j) = jset(j)*(exp(-normalized_state(normalized_state>=0)*log(sqrt(jset(j)*iset(i))))).^2;
+%         sig_h_exp(normalized_state>0,i,j) = jset(j)*(1+normalized_state(normalized_state>0)*log(sqrt(jset(j)*iset(i)))).^2;
+%         sig_h_exp(normalized_state<=0,i,j) = jset(j)*(exp(normalized_state(normalized_state<=0)*log(sqrt(jset(j)*iset(i))))).^2;
     end
 end
 
 %%
 figure('color','w'); hold on; grid on;
 j=1;
-for i = 1:length(iset)
+i=1;%for i = 1:length(iset)
     plot(x_set, sig_h_exp(:,i,j))
-end
+% end
 
 xlim(x_set([1 end]))
 ylim([0 10])
